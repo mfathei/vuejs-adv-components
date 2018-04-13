e1a8d4936fdc930f1972989f137bb4ebb3bf1db3
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 11:38:22 +0200

    121 how slot content gets compiled and styled

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12">
                <app-quote>
-                   <h2>The Quote</h2>
+                   <h2>{{ quoteTitle }}</h2>
                    <p>A wonderful Quote.</p>
                </app-quote>
             </div>
@@ -14,12 +14,19 @@
 <script>
 import Quote from "./components/Quote.vue";
 export default {
+  data() {
+    return {
+      quoteTitle: "The Quote"
+    };
+  },
   components: {
     appQuote: Quote
   }
 };
 </script>
 
-<style>
-
+<style scoped>
+h2 {
+  color: blue;
+}
 </style>

10c46edebe39981e1bfaa0c592b9f84738e6f256
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 14:04:48 +0200

    125 selecting multiple components with dynamic component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
+               <button @click="selectedComponent = 'app-quote'">Quote</button>
+               <button @click="selectedComponent = 'app-author'">Author</button>
+               <button @click="selectedComponent = 'app-new'">New</button>
+               <p>{{ selectedComponent }}</p>
+               <component :is="selectedComponent">
+                   <p>Default Content</p>
+               </component>
+               <!-- <app-quote>
                    <h2 slot="title">{{ quoteTitle }}</h2>
                    <p>A wonderful Quote.</p>
-               </app-quote>
+               </app-quote> -->
             </div>
         </div>
     </div>
@@ -13,14 +20,19 @@
 
 <script>
 import Quote from "./components/Quote.vue";
+import Author from "./components/Author.vue";
+import New from "./components/New.vue";
 export default {
   data() {
     return {
-      quoteTitle: "The Quote"
+      quoteTitle: "The Quote",
+      selectedComponent: "appQuote"
     };
   },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
 };
 </script>

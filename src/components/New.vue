a8db25e06b7e4186c443b43c1d9e83705bbe9308
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 14:10:10 +0200

    126 understanding dynamic component behavior

---
+++ src/components/New.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <button @click="counter++">Increase</button>
+    <p>{{ counter }}</p>
+</div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  destroyed() {
+    console.log("Destroyed!");
+  }
+};
+</script>
+
